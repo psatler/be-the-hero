d577b645ba5ac376055dfe94fb4bb9b527dc2105
Pablo Satler <pablosatler@gmail.com>
2020-03-25 14:15:16 -0300

    chore(backend): add deletion of incidents

--- backend/src/controllers/IncidentController.ts
+++ backend/src/controllers/IncidentController.ts
@@ -2,18 +2,49 @@ import { Request, Response } from 'express'
 import connection from '../database/connection'
 
 class IncidentController {
+  public async index (req: Request, res: Response): Promise<Response> {
+    const incidents = await connection('incidents').select('*')
+
+    return res.json(incidents)
+  }
+
   public async store (req: Request, res: Response): Promise<Response> {
     const { title, description, value } = req.body
-    const ong_id = req.headers.authorization
+    const ong_id = req.headers.authorization // eslint-disable-line
 
     const [id] = await connection('incidents').insert({
       title,
       description,
       value,
-      ong_id
+      ong_id // eslint-disable-line
     })
 
     return res.json({ id })
+  }
+
+  public async delete (req: Request, res: Response): Promise<Response> {
+    const { id } = req.params
+    const ong_id = req.headers.authorization // eslint-disable-line
+
+    const incident = await connection('incidents').where('id', id).select('ong_id').first()
+
+    console.log(incident)
+
+    if (!incident) {
+      return res.status(400).json({
+        error: 'Incident not found'
+      })
+    }
+
+    if (incident.ong_id !== ong_id) { // eslint-disable-line
+      return res.status(401).json({
+        error: 'Operation not allowed'
+      })
+    }
+
+    await connection('incidents').where('id', id).delete()
+
+    return res.status(204).send() // OK status with no content (204)
   }
 }
 
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -10,6 +10,8 @@ const routes = Router()
 routes.get('/ongs', OngController.index)
 routes.post('/ongs', OngController.store)
 
+routes.get('/incidents', IncidentController.index)
 routes.post('/incidents', IncidentController.store)
+routes.delete('/incidents/:id', IncidentController.delete)
 
 export default routes
