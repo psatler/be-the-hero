10f542737b9f14b74bd6c34b82dcf19fc93fc7de
Pablo Satler <pablosatler@gmail.com>
2020-03-25 13:58:39 -0300

    fix(backend): adjust tsconfig.json file to avoid some complaints

--- backend/src/controllers/IncidentController.ts
+++ backend/src/controllers/IncidentController.ts
@@ -1,18 +1,16 @@
 import { Request, Response } from 'express'
-// import crypto from 'crypto'
-
 import connection from '../database/connection'
 
 class IncidentController {
   public async store (req: Request, res: Response): Promise<Response> {
     const { title, description, value } = req.body
-    const ongId = req.headers.authorization
+    const ong_id = req.headers.authorization
 
     const [id] = await connection('incidents').insert({
       title,
       description,
       value,
-      ongId
+      ong_id
     })
 
     return res.json({ id })
