1f22f894529aed57c1e4e9a745ef4b945f99c6af
Pablo Satler <pablosatler@gmail.com>
2020-03-28 13:26:11 -0300

    chore(frontend): install react-icons

--- frontend-web/src/pages/Logon/index.tsx
+++ frontend-web/src/pages/Logon/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi'
 
-// import { Container } from './styles';
+import './styles.css'
+
+import heroesimg from '../../assets/heroes.png'
+import logoImg from '../../assets/logo.svg'
 
 export default function Logon() {
   return (
-    <div />
+    <div className="logon-container">
+      <img src={logoImg} alt="Be The Hero"/>
+
+      <section className="form">
+        <h1>Logon into the app</h1>
+
+        <input placeholder="Your ID" />
+        <button type="submit">Enter</button>
+
+        <a href="/register">
+          <FiLogIn size={16} color="#E02041" />
+          I'm not registered
+        </a>
+      </section>
+
+      <img src={heroesimg} alt="Heroes"/>
+    </div>
   );
 }
